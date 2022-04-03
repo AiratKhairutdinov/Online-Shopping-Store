2ea9f642affb919a53d430716f798d061fb04901
AiratKhairutdinov <airatnk93@mail.ru>
2022-04-03 20:16:56 +0300

    first commit

---
+++ frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3cllaq1fZHRF3q5d7',
+        issuer: 'https://dev-02849449.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
